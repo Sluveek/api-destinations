ceafd800524a1058a4bf2458625aa501178d93e8
Veaveslav Chirita <you@example.com>
2023-09-04 22:10:59 +0200

    annotating Profile views, querysets, edit serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,10 +8,11 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
-        if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError("Image size larger than 2MB!")
+        if value.size > 4 * 1024 * 1024:
+            raise serializers.ValidationError("Image size larger than 4MB!")
         if value.image.height > 4096:
             raise serializers.ValidationError("Image height larger than 4096px!")
         if value.image.width > 4096:
@@ -20,6 +22,13 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context["request"]
         return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            like = Like.objects.filter(owner=user, post=obj).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Post
@@ -35,4 +44,5 @@ class PostSerializer(serializers.ModelSerializer):
             "content",
             "image",
             "image_filter",
+            "like_id",
         ]
