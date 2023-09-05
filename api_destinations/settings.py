38fadc67f657cbd282f9a171b69f2fb9307fd0b2
Veaveslav Chirita <you@example.com>
2023-09-05 18:45:29 +0200

    Preparing for deployment

--- api_destinations/settings.py
+++ api_destinations/settings.py
@@ -64,7 +64,10 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-80x+qd%$o44n!nf!7!@9)0p99eq36kwo^%hqzh3$+25@qyp^b-"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-80x+qd%$o44n!nf!7!@9)0p99eq36kwo^%hqzh3$+25@qyp^b-",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
