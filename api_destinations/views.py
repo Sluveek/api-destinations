e5607f6b508d8dc1a153068827c39ab2a6fa354b
Veaveslav Chirita <you@example.com>
2023-09-09 15:21:30 +0200

    Added method get for api view test

--- api_destinations/views.py
+++ api_destinations/views.py
@@ -14,7 +14,7 @@ def root_route(request):
 
 
 # dj-rest-auth logout view fix
-@api_view(["POST", "GET")
+@api_view(["POST", "GET"])
 def logout_route(request):
     response = Response()
     response.set_cookie(
